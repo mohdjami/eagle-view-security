cb03b037ed3f91cb3bf8c59f873f71fad0710d7d
mohd jami <your_email@example.com>
2024-04-26 01:04:54 +0530

    fixing changes

--- app/emails/route.ts
+++ app/emails/route.ts
@@ -1,13 +1,6 @@
 import { Resend } from "resend";
 import { EmailTemplate } from "@/components/email-template";
 import { NextRequest, NextResponse } from "next/server";
-import sgMail from "@sendgrid/mail";
-if (!process.env.NEXT_PUBLIC_SENDGRID_API_KEY) {
-  throw new Error("SENDGRID_API_KEY environment variable is not set");
-}
-
-sgMail.setApiKey(process.env.NEXT_PUBLIC_SENDGRID_API_KEY);
-
 const resend = new Resend(process.env.NEXT_PUBLIC_RESEND_API_KEY);
 
 export async function POST(req: NextRequest, res: NextResponse) {
